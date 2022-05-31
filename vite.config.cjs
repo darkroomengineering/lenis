8a9ca2d866ff5a472408f10c5153896dc49079f8
arzafran <franco@studiofreight.com>
2022-05-31 13:18:28 -0300

    fix structure

--- vite.config.cjs
+++ vite.config.cjs
@@ -2,5 +2,8 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-  root: resolve(__dirname, 'docs/')
+  root: resolve(__dirname, 'src-docs/'),
+  build: {
+    outDir: '../docs',
+  },
 })
