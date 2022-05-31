defdf377a2397b4f68a18c8e4e2ec789b5d28f87
clementroche <rchclement@gmail.com>
2022-05-31 18:31:18 +0200

    clean up

--- vite.config.cjs
+++ vite.config.cjs
@@ -2,8 +2,5 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-  root: resolve(__dirname, 'src-docs/'),
-  build: {
-    outDir: '../docs',
-  },
+  root: resolve(__dirname, 'docs/'),
 })
