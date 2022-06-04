cb601c9c025c8b897699a711299c183cc54caa94
Clément Roche <rchclement@gmail.com>
2022-06-04 22:38:02 +0200

    nested scroll workaround

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,7 @@
 // import Lenis from '../dist/lenis.modern'
 import Lenis from '../src/lenis'
 import './styles/main.css'
+import Stats from 'stats.js'
 
 const lenis = new Lenis({ lerp: 0.1, smooth: true })
 window.lenis = lenis
@@ -9,9 +10,17 @@ lenis.on('scroll', ({ scroll }) => {
   console.log({ scroll })
 })
 
+const stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
 function raf() {
+  stats.begin();
+
   requestAnimationFrame(raf)
   lenis.raf()
+
+  stats.end();
 }
 
 raf()
