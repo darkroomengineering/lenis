06b6af61d56c7856d15a203be851ef5e217f0d8f
arzafran <franco@studiofreight.com>
2022-08-25 15:57:23 -0300

    bring bac docs website

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
   root: resolve(__dirname, 'docs/'),
-  // build: {
-  //   ssr: true,
-  //   lib: {
-  //     entry: resolve(__dirname, 'src/index.js'),
-  //     name: '@studio-freight/hamo',
-  //     fileName: 'hamo',
-  //   },
-  //   outDir: resolve(__dirname, 'dist'),
-  // },
 })
