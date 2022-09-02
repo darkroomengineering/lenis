40fe77ab0bbf0390ee8f45c0f9f36efc42b98912
arzafran <franco@studiofreight.com>
2022-09-02 19:34:29 -0300

    prefetch/preload fonts

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -6,13 +6,48 @@ export default function Document() {
     <Html lang="en" className={process.env.NODE_ENV === 'development' && 'dev'}>
       <Head>
         <meta charSet="UTF-8" />
-        {/* <link
-            href="/fonts/aeonik/AeonikPro-Bold.woff2"
-            as="font"
-            rel="preload prefetch"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          /> */}
+        <link
+          href="/fonts/Slussen-Bold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Slussen-Compressed-Black.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Slussen-Expanded-Black.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Slussen-Medium.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Slussen-Regular.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Slussen-Semibold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         {/* // https://github.com/donavon/use-dark-mode */}
