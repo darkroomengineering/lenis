e172d09d641e2042e2e67f2cff88f5b6b9dbee82
Clément Roche <rchclement@gmail.com>
2022-09-06 12:35:34 +0200

    improve enter lenis (vh)

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -211,14 +211,14 @@ export default function Home() {
       >
         <div className={s.inner}>
           <div className={s.zoom}>
-            <h2 className={cn(s.first, 'h1')}>
+            <h2 className={cn(s.first, 'h1 vh')}>
               so we built <br />
               <span className="contrast">web scrolling</span>
             </h2>
-            <h2 className={cn(s.enter, 'h3')}>
+            <h2 className={cn(s.enter, 'h3 vh')}>
               Enter <br /> Lenis
             </h2>
-            <h2 className={cn(s.second, 'h1')}>As it should be</h2>
+            <h2 className={cn(s.second, 'h1 vh')}>As it should be</h2>
           </div>
         </div>
         {/* <div className={s.enter}></div> */}
