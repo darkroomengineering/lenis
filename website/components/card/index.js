76d1eb2c4f9201c401b0f91a1478a7858c4ee64f
arzafran <franco@studiofreight.com>
2022-09-08 12:08:57 -0300

    merge

--- website/components/card/index.js
+++ website/components/card/index.js
@@ -1,10 +1,21 @@
 import cn from 'clsx'
 import s from './card.module.scss'
 
-export const Card = ({ number, text, className, inverted }) => {
+export const Card = ({
+  number,
+  text,
+  className,
+  inverted,
+  background = 'rgba(14, 14, 14, 0.15)',
+}) => {
   return (
-    <div className={cn(className, s.wrapper, inverted && s.inverted)}>
-      {number && <p className={s.number}>{number}</p>}
+    <div
+      className={cn(className, s.wrapper, inverted && s.inverted)}
+      style={{ '--background': background }}
+    >
+      {number && (
+        <p className={s.number}>{number.toString().padStart(2, '0')}</p>
+      )}
       {text && <p className={s.text}>{text}</p>}
     </div>
   )
