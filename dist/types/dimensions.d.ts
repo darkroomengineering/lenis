4ea71d734f9b93e6f45ba7d6ba2cda00f5d9b581
Clément Roche <rchclement@gmail.com>
2023-03-30 11:38:48 +0200

    v1.0.6

--- dist/types/dimensions.d.ts
+++ dist/types/dimensions.d.ts
@@ -2,16 +2,16 @@ export class Dimensions {
     constructor(wrapper: any, content: any);
     wrapper: any;
     content: any;
-    width: any;
-    height: any;
-    scrollHeight: any;
-    scrollWidth: any;
     wrapperResizeObserver: ResizeObserver;
     contentResizeObserver: ResizeObserver;
     onWindowResize: () => void;
+    width: any;
+    height: any;
     destroy(): void;
     onWrapperResize: () => void;
     onContentResize: () => void;
+    scrollHeight: any;
+    scrollWidth: any;
     get limit(): {
         x: number;
         y: number;
