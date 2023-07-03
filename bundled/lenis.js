6cd6984069f72c9909abcef41c168dd0c5b4ea35
Clément Roche <rchclement@gmail.com>
2023-07-03 14:51:19 +0200

    bump prepatch

--- bundled/lenis.js
+++ bundled/lenis.js
@@ -49,7 +49,7 @@
     return typeof key === "symbol" ? key : String(key);
   }
 
-  var version = "1.0.16";
+  var version = "1.0.17-1";
 
   // Clamp a value between a minimum and maximum value
   function clamp(min, input, max) {
