fae0fa079fe2ca0d2e5da332ddb1b2ecf0b6bca2
Clément Roche <rchclement@gmail.com>
2023-11-28 21:16:47 +0100

    1.0.29

--- bundled/lenis.js
+++ bundled/lenis.js
@@ -49,7 +49,7 @@
     return typeof key === "symbol" ? key : String(key);
   }
 
-  var version = "1.0.28";
+  var version = "1.0.29";
 
   // Clamp a value between a minimum and maximum value
   function clamp(min, input, max) {
