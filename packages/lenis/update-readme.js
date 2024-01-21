ac53e52360f889dc04638e9db0ac433d84baff31
Clément Roche <rchclement@gmail.com>
2024-01-21 14:01:44 +0100

    update readme

--- packages/lenis/update-readme.js
+++ packages/lenis/update-readme.js
@@ -3,38 +3,48 @@ const packageJson = require('./package.json')
 
 const readmePath = './README.md'
 
-// update version in README
-fs.readFile(readmePath, 'utf8', (err, data) => {
-  if (err) {
-    console.log(`Error reading README file: ${err}`)
-    return
-  }
+function updateVersion() {
+  return new Promise((resolve, reject) => {
+    // update version in README
+    fs.readFile(readmePath, 'utf8', (err, data) => {
+      if (err) {
+        console.log(`Error reading README file: ${err}`)
+        return reject(err)
+      }
 
-  const updatedReadme = data.replace(
-    /lenis@[0-9]+\.[0-9]+\.[0-9]+/i,
-    'lenis@' + packageJson.version
-  )
+      const updatedReadme = data.replace(
+        /\/lenis@([^\/]+)\//,
+        `/lenis@${packageJson.version}/`
+      )
 
-  fs.writeFile(readmePath, updatedReadme, 'utf8', (err) => {
+      fs.writeFile(readmePath, updatedReadme, 'utf8', (err) => {
+        resolve()
+
+        if (err) {
+          return reject(err)
+        }
+      })
+    })
+  })
+}
+
+function copyReadme() {
+  // copy README to root (both NPM and Github display the right README)
+  fs.readFile(readmePath, 'utf8', (err, data) => {
     if (err) {
+      console.error(`Error reading source file: ${err.message}`)
       return
     }
-  })
-})
 
-// copy README to root (both NPM and Github display the right README)
-fs.readFile(readmePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Error reading source file: ${err.message}`)
-    return
-  }
-
-  // Write the content to the destination file
-  fs.writeFile('../../README.md', data, 'utf8', (err) => {
-    if (err) {
-      console.error(`Error writing to destination file: ${err.message}`)
-    } else {
-      console.log('File copied successfully!')
-    }
+    // Write the content to the destination file
+    fs.writeFile('../../README.md', data, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to destination file: ${err.message}`)
+      } else {
+        console.log('File copied successfully!')
+      }
+    })
   })
-})
+}
+
+updateVersion().then(() => copyReadme())
