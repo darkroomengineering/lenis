3dc11c26c42a5ed52e7cff9135a4338ff8ac3e02
Clément Roche <rchclement@gmail.com>
2024-03-17 19:58:12 +0100

    @studio-freight/lenis@1.0.41

--- packages/lenis/dist/lenis.js
+++ packages/lenis/dist/lenis.js
@@ -4,7 +4,7 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Lenis = factory());
 })(this, (function () { 'use strict';
 
-  var version = "1.0.40";
+  var version = "1.0.41";
 
   // Clamp a value between a minimum and maximum value
   function clamp(min, input, max) {
