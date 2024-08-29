fa3c20dacc591185acf499919ca135963f0393ee
feledori <felix@felixmayr.it>
2024-08-29 17:36:41 +0200

    remove custom playgrounds

--- packages/vue/playground/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import lenisVue from '../dist/lenis-vue'
-import App from './App.vue'
-import './style.css'
-
-const app = createApp(App)
-app.use(lenisVue)
-
-app.mount('#app')
