338b0304af57e67319b7c72fdc5398a5e0347ce1
Clément Roche <rchclement@gmail.com>
2024-11-05 16:05:26 +0100

    remove website

--- website/components/image/index.js
+++
@@ -1,7 +0,0 @@
-import cn from 'clsx'
-import NextImage from 'next/future/image'
-import s from './image.module.scss'
-
-export function Image({ className, ...props }) {
-  return <NextImage {...props} className={cn(s.image, className)} />
-}
