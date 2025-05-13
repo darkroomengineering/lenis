b930fe8a7d7f1c1c8c76d3091e7f0ea5bb074add
Clément Roche <rchclement@gmail.com>
2025-05-13 21:34:31 +0200

    export LenisContext

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,8 +1,9 @@
 // This file serves as an entry point for the package
 export {
-  ReactLenis as Lenis,
-  ReactLenis,
   ReactLenis as default,
+  ReactLenis as Lenis,
+  LenisContext,
+  ReactLenis,
 } from './src/provider'
 export * from './src/types'
 export { useLenis } from './src/use-lenis'
